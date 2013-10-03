c5281e11e0031ff049cd955387b75c5cc158b4f3
Mohsin JK <mohsinjk@hotmail.com>
2013-10-03 23:03:33 +0200

    Improved design
    twitter bootstrap for font icons and app style.

--- _app.js
+++ _app.js
@@ -1,1 +1,35 @@
-﻿
+﻿$(document).ready( function(){
+var container=$('#container');
+var btnBack=$('#back');
+var btnFeed=$('#feed');
+var btnPeople=$('#people');
+var btnMe=$('#me');
+var btnLoding=$('#loading');
+
+var html=container.html();
+//container.html('');
+btnBack.on('click', function(){
+	window.history.back();
+});
+
+
+btnFeed.on('click', function(){
+	showContents(html);
+});
+
+btnPeople.on('click', function(){
+	showContents(html);
+});
+
+btnMe.on('click', function(){
+	showContents(html);
+});
+
+var showContents=function( data ){
+	btnLoding.fadeIn("slow", function(){
+	container.html(html).fadeIn("slow");
+	btnLoding.fadeOut("slow");
+	});
+};
+
+});
